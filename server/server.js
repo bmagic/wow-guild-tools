f844c1dfb918d4010809e1e9309db475548faf9a
Azinya <bnet.azinya@gmail.com>
2015-03-14 19:12:43 +0100

    Auth modifications - to debug

--- server/server.js
+++ server/server.js
@@ -42,9 +42,7 @@ var cronJob = require('cron').CronJob;
 new cronJob('0 0 * * * *',feeds.fetchFeeds,null, true);
 feeds.fetchFeeds();
 
-
-
-
+// Declarations
 var optionsPhpbb3 = {
     host: config.mysql_phpbb3.host,
     port: config.mysql_phpbb3.port,
@@ -54,7 +52,6 @@ var optionsPhpbb3 = {
     createDatabaseTable: true
 }
 var connectionPhpbb3 = mysql.createConnection(optionsPhpbb3);
-
 
 app.use(session({
     key: 'gt.sid',
@@ -72,37 +69,55 @@ app.use(passport.session());
 
 server.listen(3000);
 
-
+// App root
 app.use(function(req, res, next) {
     if (req.isAuthenticated() ||
         req.path.indexOf('/login') != -1 ||
         req.path.indexOf('.css') != -1 ||
         req.path.indexOf('.js') != -1
     ) { return next(); }
+    
+    connectionPhpbb3.query('SELECT config_value from `phpbb_config` where config_name=?', ['cookie_name'], function(err, rows, fields) {
+        if (!err && rows[0] && req.cookies[rows[0].config_value+"_sid"]) {
+	    console.log('auth by cookie')
+	    req.body.username = "azinya"
+	    req.body.password = "tatayoyo"
+	    console.log(req.body)
+	    passport.authenticate('form_auth', { });
+	} else {
+	    res.redirect('/login.html');
+        }
+    });
 
-    res.redirect('/login.html');
 
 });
 app.use(express.static(path.join(__dirname ,'../app')));
 
+// Authentication
 app.post('/login',
-    passport.authenticate('local', {
+    passport.authenticate('form_auth', {
         successRedirect: '/',
-        failureRedirect: '/login.html'
+        failureRedirect: '/login.html',
+        failureMessage: true
     })
 );
 
 app.get('/logout', function(req, res){
-
     req.logout();
-    res.redirect('/');
+    res.redirect('/login.html');
 });
 
-
-passport.use(new LocalStrategy(
+passport.use('phpbb_cookie', new LocalStrategy(
     function(username, password, done) {
-        var username_clean = connectionPhpbb3.escape(username.toLowerCase());
-        connectionPhpbb3.query('SELECT user_id, user_password from `phpbb_users` where `username_clean`='+username_clean, function(err, rows, fields) {
+	console.log('toto')
+        return done(null, {"uid":0,"username":'titi',"role":'officier'});
+    }
+));
+
+passport.use('form_auth', new LocalStrategy(
+    function(username, password, done) {
+        var username_clean = username.toLowerCase();
+        res = connectionPhpbb3.query('SELECT user_id, user_password from `phpbb_users` where username_clean=?',[username_clean], function(err, rows, fields) {
             if (err) return done(err);
 
             if (rows.length == 0)
@@ -113,44 +128,23 @@ passport.use(new LocalStrategy(
             if (!bcrypt.compareSync(password, user_password))
                 return done(null, false, { message: 'Mot de passe ou utilisateur incorrect' });
 
-
             var uid = rows[0].user_id;
-
-            connectionPhpbb3.query('SELECT group_id from `phpbb_user_group` where `user_id`='+uid, function(err, rows, fields) {
-
-                var role = null;
-                var officier = false;
-                var membre = false;
-                var apply = false;
-                var casu = false;
+            connectionPhpbb3.query('SELECT group_id from `phpbb_user_group` where `user_id`=?', [uid], function(err, rows, fields) {
+		if (err) return done(err);
+		var roles = []
                 rows.forEach(function(item){
-                    if (item.group_id == config.roles.officier)
-                        officier = true;
-                    if (item.group_id == config.roles.membre)
-                        membre = true;
-                    if (item.group_id == config.roles.apply)
-                        apply = true;
-                    if (item.group_id == config.roles.casu)
-                        casu = true;
+		    config.roles.forEach(function(d,i){
+                        if (item.group_id == d.phpbb_id) roles.push(i)
+		    });
                 });
-
-                if (officier == true)
-                    role = "officier";
-                else if (membre == true)
-                    role = "membre";
-                else if (apply == true)
-                    role = "apply";
-                else if (casu == true)
-                    role = "casu";
-                else
-                    return done(null, false, { message: 'Permissions insuffisantes' });
-
+		if (roles.length == 0) return done(null, false, { message: 'Permissions insuffisantes' });
+		var role = config.roles[Math.min.apply(null,roles)].name
                 return done(null, {"uid":uid,"username":username,"role":role});
             });
         });
+	return res
     }
 ));
-
 
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -159,7 +153,6 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(obj, done) {
     done(null, obj);
 });
-
 
 io.use(passportSocketIo.authorize({
     cookieParser: cookieParser,
